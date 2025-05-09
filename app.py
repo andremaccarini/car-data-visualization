cca08550d6cc0624fcacb9b77cdf817aec5fbc5c
Andre <coralmacca@gmail.com>
2025-05-09 14:40:07 -0300

    Update

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 import plotly.express as px
 import os
 
 
 file_path = os.path.join('Data', 'vehicles.csv')
 car_data = pd.read_csv(file_path)
+
+st.title("Car Data Analysis")
+st.write("This is a simple web application to analyze car data.")
+
 
 hist_button = st.checkbox('Show Histogram')
 if hist_button:
